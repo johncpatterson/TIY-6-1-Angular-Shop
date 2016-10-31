99e9ee05c622fca29c1dbab3a5d7c35af6575112
[John Patterson] <pattersonjc1@gmail.com>
2016-10-31 15:55:10 -0400

    initial functionality in place

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# TIY-3-4-Weather-App
+# TIY-6-1-Angular Shop
 
-The Iron Yard week 3, day 3 assignment.  The goal of this assignment was to build a basic weather app using the Open Weather Data API to pull a forecast for a city and display the 7 day forecast with high and low temperatures as well as conditions for each day.
+The Iron Yard week 6, day 1 assignment.  For this assignment I built a basic shop app using Angular to learn Angular controllers and modules.  I used a sample data set and displayed the data in a table using Angular.
--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
-jQuery(function() {
+(function() {
+    'use strict';
 
-    // $("#submitForm").on("submit", function() {
-    //     event.preventDefault();
-    //     var name = $("#nameBox").val();
-    //     var meal = $("#textBox").val();
-    //     var restaurant = $("#restBox").val();
-    //     getName(name, meal, restaurant);
+    angular
+        .module('shop', [])
+        .controller('StockController', function StockController() {
+            this.tax = .0575;
+            this.items = [
+                { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
+                { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
+                { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
+                { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
+                { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
+                { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
+                { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
+                { "id": 46, "name": "guitar", "price": 899, "quantity": 0, "color": "blue", "discount": 150 },
+                { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
+                { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
+                { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
+                { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
+            ];
+            this.discountIcon = false;
 
-    // })
-    // $("#button").on("click", function() {
-    //     var name = $("#nameBox").val();
-    //     var meal = $("#textBox").val();
-    //     var restaurant = $("#restBox").val();
-    //     getName(name, meal, restaurant);
+            this.figureSalesTax = function (price, discount) {
+                
+                var subTotal = price - discount;
+                var tax = subTotal * this.tax;
+                var total = subTotal + tax;
+                console.log(total);
+                return total;
+                
+            };
 
-    // })
+            // if (items.discount > 0) {
+            //     $('#price-with-discount').append(`
+            //         <img src="https://tiy-learn-content.s3.amazonaws.com/98350f05-sale_2.png">
+            //         `)
+            // } 
+
+            // ngShow
 
 
-    // function getName(name, meal, restaurant) {
-    //     $.ajax({
-    //         method: 'POST',
-    //         data: { name: name, meal: meal, restaurant: restaurant },
-    //         url: "http://192.168.1.8:3005/doSomething",
-    //         success: function(result) {},
-    //     });
-    // }
-
-// end of jQuery function tag:
-});
+        });
+})();
